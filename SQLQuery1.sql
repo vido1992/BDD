e23d4be550047b7ba99e4f4116fb032f8c617a40
vido <david.moreno01@epn.edu.ec>
2021-01-19 14:51:10 -0500

    actualizacion

---
+++ SQLQuery1.sql
@@ -0,0 +1,34 @@
+select * from persona
+
+insert into persona values(1,'David','Moreno','tu direccion',1234567891),
+					values(1,'David','Moreno','tu direccion',1234567891),
+					values(1,'David','Moreno','tu direccion',1234567891),
+					values(1,'David','Moreno','tu direccion',1234567891),
+					values(1,'David','Moreno','tu direccion',1234567891),
+					values(1,'David','Moreno','tu direccion',1234567891);
+
+select * from usuario
+
+insert into usuario values(1,1,'login','pasw','A'),
+					values(1,1,'login','pasw','B'),
+					values(1,1,'login','pasw','P'),
+					values(1,1,'login','pasw','I');
+					
+select * from proveedores
+
+insert into proveedores values(1,'nombre','direccion',1234567890),
+						values(1,'nombre','direccion',1234567890),
+						values(1,'nombre','direccion',1234567890),
+						values(1,'nombre','direccion',1234567890);
+						
+select * from producto
+
+insert into producto values (1,'producto','descripcion',12.30,10,06/01/2022)
+					 values (1,'producto','descripcion',12.30,10,06/01/2022)
+					 values (1,'producto','descripcion',12.30,10,06/01/2022)
+					 
+select * from sector
+
+select * from solicitudproduccion
+
+insert into solicitudproduccion values(1,2,1)
